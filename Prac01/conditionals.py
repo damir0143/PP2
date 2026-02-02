b335b63858d3107fcaa0e4f9a9b057142220cf7f
damir0143 <d_mashabaev@kbtu.kz>
2026-02-02 15:15:59 +0500

    conditionals completed

--- Prac01/conditionals.py
+++ Prac01/conditionals.py
@@ -1,6 +1,34 @@
-
+#1
 a = 5
 if a%2 == 0:
     print("even")
 else:
     print ("odd")
+#2
+age = 18
+
+if age >= 18:
+    print("совершеннолетний")
+#3
+score = 85
+
+if score >= 90:
+    print("Оценка: 5")
+elif score >= 70:
+    print("Оценка: 4")
+else:
+    print("Оценка: 3")
+#4
+password = "1234"
+
+if password == "1234":
+    print("Доступ разрешён")
+else:
+    print("Неверный пароль")
+#5
+text = ""
+
+if text:
+    print("Строка не пустая")
+else:
+    print("Строка пустая")
