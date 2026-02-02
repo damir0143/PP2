2ce6cd8530b8b9dc37b536ebbf080be9352b4579
damir0143 <d_mashabaev@kbtu.kz>
2026-02-02 15:11:53 +0500

    strings completed

--- Prac01/strings.py
+++ Prac01/strings.py
@@ -3,3 +3,14 @@ print("hello")
 #2
 s = "awesome"
 print(s)
+#3 
+word = "кот"
+print(word[0])  
+print(word[1])  
+#4
+word = "Python"
+print(len(word))
+#5
+name = "Дамир"
+greeting = "Привет, " + name
+print(greeting)
