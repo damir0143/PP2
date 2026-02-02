6e335d59a20deb476a7063e198037d8ed24f672c
damir0143 <d_mashabaev@kbtu.kz>
2026-02-02 15:27:13 +0500

    variables completed

---
+++ Prac01/variables.py
@@ -0,0 +1,21 @@
+#1
+x = 5
+y = "John"
+print(x)
+print(y)
+#2
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+#3
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+#4
+age = 20
+print(age)
+#5
+a = 5
+b = 3
+sum = a + b
+print(sum)
