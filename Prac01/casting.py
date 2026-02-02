9f83806c7f1d06231dff8361be9df33bcfc74449
damir0143 <d_mashabaev@kbtu.kz>
2026-02-02 15:37:49 +0500

    castings completed

---
+++ Prac01/casting.py
@@ -0,0 +1,22 @@
+#!
+age_str = "18"
+age = int(age_str)
+print(age)
+#2
+year = 2026
+year_str = str(year)
+print(year_str)
+#3
+number = 5
+number_float = float(number)
+print(number_float)
+#4
+price = 9.8
+price_int = int(price)
+print(price_int)
+
+#5
+print(bool(0))      # False
+print(bool(1))      # True
+print(bool(""))     # False
+print(bool("Hi"))   # True
