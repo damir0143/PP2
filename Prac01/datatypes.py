dfb397f1284b7307ca6e76e37d57f85789e0f4a8
damir0143 <d_mashabaev@kbtu.kz>
2026-02-02 15:29:13 +0500

    data types completed

---
+++ Prac01/datatypes.py
@@ -0,0 +1,15 @@
+#1
+age = 25
+print(age)
+#2
+price = 19.99
+print(price)
+#3
+text = "Hello, Python!"
+print(text)
+#4
+is_student = True
+print(is_student)
+#5
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
