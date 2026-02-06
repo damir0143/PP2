e33c9e96d39cfb22dfcaf3801615ed2a904e5443
damir0143 <d_mashabaev@kbtu.kz>
2026-02-06 11:17:48 +0500

    added examples if_elif_else

---
+++ Prac02/if_else/if_elif_else.py
@@ -0,0 +1,50 @@
+# 1
+x = 0
+if x > 0:
+    print("Положительное число")
+elif x < 0:
+    print("Отрицательное число")
+else:
+    print("Ноль")
+
+# 2
+marks = 85
+if marks >= 90:
+    print("Отлично")
+elif marks >= 75:
+    print("Хорошо")
+elif marks >= 50:
+    print("Удовлетворительно")
+else:
+    print("Неуд")
+
+#3
+day = "Суббота"
+if day == "Суббота" or day == "Воскресенье":
+    print("Выходной")
+else:
+    print("Рабочий день")
+
+# Пример 4
+temperature = 10
+if temperature > 30:
+    print("Очень жарко")
+elif temperature > 20:
+    print("Тепло")
+elif temperature > 10:
+    print("Прохладно")
+else:
+    print("Холодно")
+
+#5
+score = 75
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
