e59cb5b71ef3684138c3e6970ed56bd6be241751
damir0143 <d_mashabaev@kbtu.kz>
2026-02-06 11:13:07 +0500

    if statement examples added

---
+++ Prac02/if_else/if_statement.py
@@ -0,0 +1,25 @@
+#1
+x = 10
+if x > 5:
+    print("x больше 5")
+
+#2
+y = -3
+if y < 0:
+    print("y отрицательное число")
+
+#3
+name = "Alice"
+if name == "Alice":
+    print("Привет, Alice!")
+
+#4
+age = 18
+if age >= 18:
+    print("Взрослый человек")
+
+#5
+
+temperature = 30
+if temperature > 25:
+    print("Сегодня жарко")
