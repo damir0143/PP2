422127d2d08dc5decfdda77d81f15db843e7b2c8
damir0143 <d_mashabaev@kbtu.kz>
2026-02-06 11:19:05 +0500

    added examples for short_hand_if

---
+++ Prac02/if_else/short_hand_if.py
@@ -0,0 +1,20 @@
+#1
+x = 5
+if x > 0: print("Положительное число")
+
+#2
+y = 10
+print("Четное") if y % 2 == 0 else print("Нечетное")
+
+#3
+age = 18
+status = "Взрослый" if age >= 18 else "Ребенок"
+print(status)
+
+#4
+name = "Alice"
+print("Привет, Alice!") if name == "Alice" else print("Привет, незнакомец!")
+
+#5
+temperature = 28
+print("Жарко") if temperature > 25 else print("Не жарко")
