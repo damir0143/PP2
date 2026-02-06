8239b8ca64b7770313920038b247c78a23977e3a
damir0143 <d_mashabaev@kbtu.kz>
2026-02-06 14:16:10 +0500

    added examples for switch

---
+++ Prac02/if_else/Switch.py
@@ -0,0 +1,58 @@
+#1
+day = 2
+
+if day == 1:
+    print("Понедельник")
+elif day == 2:
+    print("Вторник")
+elif day == 3:
+    print("Среда")
+else:
+    print("Другое")
+
+
+#2
+color = "желтый"
+
+if color == "красный":
+    print("Стоп!")
+elif color == "желтый":
+    print("Приготовься")
+else:
+    print("Вперед!")
+
+#3
+op = "+"
+a, b = 5, 3
+
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+else:
+    print("Неизвестная операция")
+
+#4
+age = 20
+
+if age < 13:
+    print("Ребёнок")
+elif age < 20:
+    print("Подросток")
+else:
+    print("Взрослый")
+
+
+#5
+score = 72
+
+if score >= 90:
+    print("Отлично")
+elif score >= 75:
+    print("Хорошо")
+elif score >= 60:
+    print("Удовлетворительно")
+else:
+    print("Неудовлетворительно")
