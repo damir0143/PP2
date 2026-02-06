91639aa8b90c9b59b6e9f6401089030047d44f2b
damir0143 <d_mashabaev@kbtu.kz>
2026-02-06 11:28:17 +0500

    asdasd

---
+++ Prac02/loops/for_loop.py
@@ -0,0 +1,23 @@
+#1
+for i in range(5):
+    print(i)
+
+#2
+for letter in "Python":
+    print(letter)
+
+#3
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+#4
+for i in range(2, 10, 2):
+    print(i)
+
+#5
+numbers = [1, 2, 3, 4, 5]
+squares = []
+for num in numbers:
+    squares.append(num**2)
+print("Квадраты:", squares)
