dc01b11d9568fd5cc4ce06ab99c3a64b2ef688c8
damir0143 <d_mashabaev@kbtu.kz>
2026-02-06 11:15:54 +0500

    added ifelse examples

---
+++ Prac02/if_else/if_else.py
@@ -0,0 +1,34 @@
+#1
+x = 4
+if x % 2 == 0:
+    print("Четное число")
+else:
+    print("Нечетное число")
+
+#2
+y = -7
+if y >= 0:
+    print("Положительное число")
+else:
+    print("Отрицательное число")
+
+#3
+name = "Bob"
+if name == "Alice":
+    print("Привет, Alice!")
+else:
+    print("Ты не Alice")
+
+#4
+age = 16
+if age >= 18:
+    print("Взрослый человек")
+else:
+    print("Еще ребенок")
+
+# 5
+temperature = 15
+if temperature > 25:
+    print("Сегодня жарко")
+else:
+    print("Сегодня не жарко")
