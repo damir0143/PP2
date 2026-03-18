9cc69324b6cbad51d888827e63eb4bbf74997dc3
damir0143 <d_mashabaev@kbtu.kz>
2026-03-18 11:09:29 +0500

    asad

--- Prac05/receipt_parser.py
+++ Prac05/receipt_parser.py
@@ -4,28 +4,27 @@ import json
 with open("raw.txt", "r", encoding="utf-8") as file:
     text = file.read()
 
-# -------------------------
-# 1. Extract prices (строки где есть x)
-# -------------------------
+
+# 1.  prices 
+
 price_pattern = r"x\s([\d\s]+,\d{2})"
 prices_raw = re.findall(price_pattern, text)
 
 prices = [float(p.replace(" ", "").replace(",", ".")) for p in prices_raw]
 
-# -------------------------
-# 2. Extract product names
-# -------------------------
+
+# 2 product names
+
 product_pattern = r"\d+\.\n(.+)"
 products = re.findall(product_pattern, text)
 
-# -------------------------
-# 3. Calculate total
-# -------------------------
+
+# 3 total price
+
 total = sum(prices)
 
-# -------------------------
-# 4. Extract date and time
-# -------------------------
+# 4 date and time
+
 datetime_pattern = r"\d{2}\.\d{2}\.\d{4}\s\d{2}:\d{2}:\d{2}"
 datetime_match = re.search(datetime_pattern, text)
 
@@ -36,17 +35,15 @@ if datetime_match:
     date_time = datetime_match.group()
     date, time = date_time.split()
 
-# -------------------------
 # 5. Payment method
-# -------------------------
+
 payment_pattern = r"Банковская карта|Наличные"
 payment_match = re.search(payment_pattern, text)
 
 payment_method = payment_match.group() if payment_match else "Unknown"
 
-# -------------------------
-# 6. Structured output
-# -------------------------
+# 6 output
+
 result = {
     "products": products,
     "prices": prices,
